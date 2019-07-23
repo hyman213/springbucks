318e439d5dea5753904d7f737111cd7c6909dec2
huhan <hyman213@163.com>
2019-07-23 22:24:24 +0800

    add customer-service

---
+++ customer-service/src/main/java/io/hyman/spring/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,60 @@
+package io.hyman.spring.customerservice;
+
+import io.hyman.spring.customerservice.model.Coffee;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.Banner;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.math.BigDecimal;
+import java.net.URI;
+
+@SpringBootApplication
+@Slf4j
+public class CustomerServiceApplication implements ApplicationRunner {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public static void main(String[] args) {
+//        SpringApplication.run(CustomerServiceApplication.class, args);
+        new SpringApplicationBuilder()
+                .sources(CustomerServiceApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .web(WebApplicationType.NONE)
+                .run(args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080/coffee/{id}").build(1);
+        ResponseEntity<Coffee> c = restTemplate.getForEntity(url, Coffee.class);
+        log.info("Response Status: {}, Response Headers: {}", c.getStatusCode(), c.getHeaders().toString());
+        log.info("Coffee: {}", c.getBody());
+
+        // 最后的"/"不能省
+        String coffeeUri = "http://localhost:8080/coffee/";
+        Coffee americano = Coffee.builder()
+                .name("Americano")
+                .price(BigDecimal.valueOf(25.00)).build();
+        ResponseEntity<Coffee> response = restTemplate.postForEntity(coffeeUri, americano, Coffee.class);
+        log.info("New Coffee: {}", response);
+
+        String s = restTemplate.getForObject(coffeeUri, String.class);
+        log.info(s);
+    }
+}
